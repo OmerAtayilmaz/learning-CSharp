9cd273a90acfd445eca5f3b396d4c4cefffd02e3
OmerAtayilmaz <omeratayilmaz@gmail.com>
2022-10-26 12:24:33 +0300

    Week 3: enums,arrays and ready-to-use functions

---
+++ Arrays/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Arrays;
+
+public class Arrays
+{
+    static double getAverage(int[] arr)
+    {
+        int i;
+        double avg;
+        int sum = 0;
+        for (i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+        avg = (double)sum / arr.Length;
+        return avg;
+    }
+    
+    static void Main()
+    {
+        int[] arr = new int[] { 1, 2, 3, 4, 5 };
+        double getAvg = getAverage(arr);
+        Console.WriteLine("{0}",getAvg);
+        
+        
+        /*Hazır Array Fonksiyonları*/
+        int[] list = { 34, 72, 13, 44, 25, 30, 10 };
+      //  int[] temp = list;
+        Console.WriteLine("Original Array\n");
+        foreach (int val in list)
+        {
+            Console.Write(val+" ");
+        }
+        Array.Reverse(list);
+        Console.WriteLine("\nReversed Array\n");
+        foreach (int val in list)
+        {
+            Console.Write(val+" ");
+        }
+        Console.WriteLine();
+        Array.Sort(list);
+        Console.WriteLine(list[list.Length-1]);
+        Console.Write("Biggest Number is: {0} Square of it : {1} ",list[list.Length-1],Math.Pow(list[list.Length-1],2));
+
+    }
+}
---
+++ week3/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace week5
+{
+    class Week5
+    {
+        static void Main(String[] args)
+        {
+            var i =10;
+            Console.WriteLine("type of i:{0}",i.GetType().ToString());
+            int[] arr={1,2,3,4,5,6,7,8,9,0,10,11,12,13,14,15,16,17,18};
+            for(int x=0;x<arr.Length;x++){
+                Console.WriteLine("Array Value is: {0} Type of Value is: {1}",x,x.GetType().ToString());
+            }
+            int[] marks=new int[5]{1,2,3,4,5};
+            //eger butun elemanlar belirtildiyse, size boş verilebilir
+            int[] marks2=new int[]{1,2,3,4,5};
+            int[] ages = new int[10];
+            for(int a=0;a<10;a++)
+            {
+                ages[a] = i+100;
+            }
+            for (int j = 0; j < 10; j++)
+            {
+                Console.WriteLine("Element[{0}] = {1}",j,ages[j]);
+            }
+            
+            
+            
+        }
+    }
+}
